72d31bb6effb57ac9bbcc79ec419669ef1b09847
Coders Club <hi@codersclub.com>
2025-04-07 01:48:24 +0300

    US_306 started

---
+++ src/US_306.java
@@ -0,0 +1,11 @@
+import Utlity.BaseDriver;
+import org.junit.Test;
+
+public class US_306 extends BaseDriver {
+    @Test
+
+    public void communicationMessageSendingProcess() {
+
+        driver.get("https://shopdemo.e-junkie.com/");
+    }
+}
