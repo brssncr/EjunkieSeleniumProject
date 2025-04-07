1cb1b560cf633c87c9da2842d190a66a3a4cd3e7
AktsT <akts.tlga@gmail.com>
2025-04-07 15:36:41 +0300

    US_308 Created. Adjusted methods of BaseDriver, Navigated to the E-junkie page.

--- src/US_308.java
+++ src/US_308.java
@@ -1,2 +1,19 @@
-public class US_308 {
+import Utlity.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US_308 extends BaseDriver {
+    @Test
+    public void openingTheWebsite(){
+        driver.get("https://shopdemo.fatfreeshop.com/?");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("EJ-ShopLink")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("EJ-ShopLink")));
+        WebElement eCommerceButton=driver.findElement(By.className("EJ-ShopLink"));
+        actionDriver.scrollToElement(eCommerceButton).moveToElement(eCommerceButton).click().build().perform();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://www.e-junkie.com");
+    }
 }
