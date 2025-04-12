a559c26f7d96776e1e74493d24cd2d41a54b801d
meertsarac <meertsarac@gmail.com>
2025-04-12 09:48:57 +0300

    Click on 'view product' button for Demo eBook

--- src/US_305.java
+++ src/US_305.java
@@ -1,9 +1,22 @@
 import Utlity.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class US_305 extends BaseDriver {
     @Test
     public void paymentProcessApprocalAndDowload() {
         driver.get("https://shopdemo.fatfreeshop.com/?");
+
+        List<WebElement> boxes = driver.findElements(By.cssSelector("div.box"));
+
+        for (WebElement box : boxes) {
+            if (box.findElement(By.cssSelector("h4.title")).getText().equals("Demo eBook")) {
+                box.findElement(By.cssSelector("button.view_product")).click();
+                break;
+            }
+        }
     }
 }
